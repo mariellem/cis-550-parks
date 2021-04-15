c9cad09a5991088eeedc25fb847ac5bc6d99ec4c
Diane Hoffstetter <dhoffstetter@mac.com>
2021-04-14 17:14:09 -0700

    Updated Main

--- Website/client/src/components/Main.js
+++ Website/client/src/components/Main.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import PageNavbar from './PageNavbar';
-import ParkDetailRow from './ParkDetailRow';
-import ParkInfoBox from './ParkInfoBox';
-import ParkSummary from './ParkSummary';
-import ParkAttendance from './ParkAttendance';
-import ParkReviewRow from './ParkReviewRow';
-import ParkWeatherRow from './ParkWeatherRow';
 import '../style/Main.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -22,6 +16,8 @@ export default class NPFinder extends React.Component {
 			parkDetail: [],
 			parkReviews: [],
 			park5DayWeathers: [],
+			regions: [],
+			selectedRegion: "",
 			opacity: 0
 		};
 
@@ -30,6 +26,11 @@ export default class NPFinder extends React.Component {
 	}
 
 	componentDidMount() {
+
+		let regionList = ["A", "B"];
+		this.setState({
+			regions: regionList
+		});
 
 		// Send an HTTP request to the server.
 		fetch("http://localhost:8081/parkNames",
@@ -59,20 +60,20 @@ export default class NPFinder extends React.Component {
 			this.submitPark()
 		}
 		
+		
 	}
 
 	handleChange(e) {
 		this.setState({
-			selectedPark: e.target.value
+			selectedRegion: e.target.value
 		});
 	}
 
-	/* ---- Q3b (Best Genres) ---- */
 	submitPark() {
 		console.log("Submitted the park")
-		console.log(this.state.selectedPark)
-		console.log(typeof(this.state.selectedPark))
-		let parkInput = this.state.selectedPark;
+		console.log(this.state.selectedRegion)
+		console.log(typeof(this.state.selectedRegion))
+		let parkInput = this.state.selectedRegion;
 		if (parkInput == "true" || parkInput == null) {
 			this.setState({
 				opactiy: 0
@@ -84,159 +85,33 @@ export default class NPFinder extends React.Component {
 		}
 		console.log("opacity is:")
 		console.log(this.state.opacity)
-		/*
-		fetch("http://localhost:8081/parkDetails/" + parkInput,
-		{
-			method: "GET"
-		}).then(res => {
-			return res.json();
-		}, err => {
-			console.log(err);
-		}).then(ParkList => {
-			console.log(ParkList); //displays your JSON object in the console
-    	let parkDivs = ParkList.map((parkDetailObj, i) => 
-       	 <ParkDetailRow name={parkDetailObj.name} address={parkDetailObj.address} phoneNumber={parkDetailObj.phoneNumber} rating={parkDetailObj.rating} totalRatings = {parkDetailObj.totalRatings}/>
-    	);
-		//This saves our HTML representation of the data into the state, which we can call in our render function
-		this.setState({
-			parkDetail: parkDivs
-		});
-    	}, err => {
-     	 // Print the error if there is one.
-     	 console.log(err);
-    	});
-		*/
 
 
-		//Fetch the park detail information and photo and save it to park photo
-		let image2 = "/public/bg.jpg"
+				//Fetch the park reviews
+				// fetch("http://localhost:8081/parkReviews/" + parkInput,
+				// {
+				// 	method: "GET"
+				// }).then(res => {
+				// 	return res.json();
+				// }, err => {
+				// 	console.log(err);
+				// }).then(parkReviews => {
+				// 	console.log(parkReviews);
+				// 	//save the image and park details to a park info box object
+				// 	let parkReview = parkReviews.map((parkRevObj, i) =>
+				// 	<ParkReviewRow date = {parkRevObj.reviewDate} rating={parkRevObj.rating} review={parkRevObj.review}/>
+				// 	);
+				// 	//update the state to have the park image
+				// 	this.setState({
+				// 		parkReviews: parkReview
+				// 	})
+					
 		
-		fetch("http://localhost:8081/photos/" + parkInput,
-		{
-			method: "GET"
-		}).then(res => {
-			return res.json();
-		}, err => {
-			console.log(err);
-		}).then(parkImage => {
-			console.log(parkImage);
-			//save the image and park details to a park info box object
-			let parkImg = parkImage.map((imageObj, i) =>
-			(image2 = imageObj.image1loc,
-			<ParkInfoBox imageUrl={imageObj.image2loc} credit={imageObj.image2credit} name = {imageObj.name} phone={imageObj.phoneNumber} rating={imageObj.rating} location={imageObj.address} lat={imageObj.lat} lng={imageObj.lng} website={imageObj.websiteUrl}/>)
-			
-			);
-			//update the state to have the park image
-			this.setState({
-				parkPhoto: parkImg,
-				imageLink: image2
-			})
-			console.log("Reset image Link")
-			console.log(this.state.imageLink)
-
-		}, 
-		err => {
-			console.log(err)
-		});
-
-
-		//Fetch the nearby park information
-		fetch("http://localhost:8081/nearbyParks/" + parkInput,
-		{
-			method: "GET"
-		}).then(res => {
-			return res.json();
-		}, err => {
-			console.log(err);
-		}).then(nearbyParks => {
-			console.log(nearbyParks);
-			//save the image and park details for the nearby parks
-			let nearbyPark = nearbyParks.map((parkObj, i) =>
-			<ParkSummary imageUrl={parkObj.image1loc} credit={parkObj.image1credit} name = {parkObj.nearbyPark} rating={parkObj.rating} distance={parkObj.distanceInMiles}/>
-			);
-			//update the state to have the park image
-			this.setState({
-				nearbyParks: nearbyPark
-			})
-			
-
-		}, 
-		err => {
-			console.log(err)
-		});
-
-
-		//Fetch the park attendance information
-		fetch("http://localhost:8081/parkAttendance/" + parkInput,
-		{
-			method: "GET"
-		}).then(res => {
-			return res.json();
-		}, err => {
-			console.log(err);
-		}).then(parkVisitors => {
-			//save the image and park details to a park info box object
-			let attendance = parkVisitors.map((parkObj, i) =>
-			<ParkAttendance year={parkObj.year} visitors={parkObj.visitors}/>
-			);
-			//update the state to have the park image
-			this.setState({
-				parkAttendance: attendance
-			})
-			console.log(this.state.parkAttendance)
-
-		}, 
-		err => {
-			console.log(err)
-		});
-
-		//Fetch the park reviews
-		fetch("http://localhost:8081/parkReviews/" + parkInput,
-		{
-			method: "GET"
-		}).then(res => {
-			return res.json();
-		}, err => {
-			console.log(err);
-		}).then(parkReviews => {
-			console.log(parkReviews);
-			//save the image and park details to a park info box object
-			let parkReview = parkReviews.map((parkRevObj, i) =>
-			<ParkReviewRow date = {parkRevObj.reviewDate} rating={parkRevObj.rating} review={parkRevObj.review}/>
-			);
-			//update the state to have the park image
-			this.setState({
-				parkReviews: parkReview
-			})
-			
-
-		}, 
-		err => {
-			console.log(err)
-		});
-
-		// Fetch the park 5 day weather
-		fetch("http://localhost:8081/park5DayWeather/" + parkInput,
-		{
-			method: "GET"
-		}).then(res => {
-			return res.json();
-		}, err => {
-			console.log(err);
-		}).then(park5DayWeathers => {
-			console.log(park5DayWeathers);
-			let park5DW = park5DayWeathers.map((park5DWObj, i) =>
-			<ParkWeatherRow mon = {park5DWObj.mon} dt={park5DWObj.dt} minTemp={park5DWObj.minTemp} maxTemp={park5DWObj.maxTemp} aveTemp={park5DWObj.aveTemp}/>
-			);
-			this.setState({
-				park5DayWeathers: park5DW
-			})
-			
-
-		}, 
-		err => {
-			console.log(err)
-		});
+				// }, 
+				// err => {
+				// 	console.log(err)
+				// });
+		
 
 
 
@@ -272,9 +147,10 @@ Recommendations will provide you some insight into things to consider when you s
 
 Wherever your adventure leaves you wandering, enjoy and keep safe!</td> 
 					<td><div className="dropdown-container">
-					<select value={this.state.selectedPark} onChange={this.handleChange} className="dropdown" id="parksDropdown">
+					<select value={this.state.selectedRegion} onChange={this.handleChange} className="dropdown" id="parksDropdown">
 						<option select value> -- select an option -- </option>
-						{this.state.parks}
+						<option value="1"> A </option>
+						<option value="2"> B </option>
 					</select>
 					<button className="submit-btn" id="parkSubmitBtn" onClick={this.submitPark}>Submit</button>
 				  </div></td> 
@@ -394,19 +270,3 @@ Wherever your adventure leaves you wandering, enjoy and keep safe!</td>
 		);
 	}
 }
-
-/*
-<div className="jumbotron2">
-				<div className="parks-container">
-				  <div className="park">
-					<div className="header"><strong>Park</strong></div>
-					<div className="header"><strong>Address</strong></div>
-					<div className="header"><strong>Phone Number</strong></div>
-					<div className="header"><strong>Rating</strong></div>
-				  </div>
-				  <div className="parks-container" id="parkResults">
-					{this.state.parkDetail}
-				  </div>
-				</div>
-			  </div>
-			  */
